7a3f81abbd933fda91872a9795e5b8acbb1fc316
Kirill Sokolov <aqua_agera_ls4@mail.ru>
2021-10-31 18:52:24 +0300

    Некоторые методы для работы с коллекциями

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
 public class Main {
@@ -13,7 +15,7 @@ public class Main {
         System.out.println(integerList);
 
 
-        var find =Collections.binarySearch(integerList,-34);
+        var find = Collections.binarySearch(integerList, -34);
         System.out.println(find);
 
 
@@ -22,52 +24,47 @@ public class Main {
         System.out.println(integerList);
 
 
-
         // Перетасовка объектов листа
-        Collections.shuffle(integerList,new Random());
+        Collections.shuffle(integerList, new Random());
         System.out.println(integerList);
 
-        Person person = new Person("Kirill",22);
-        Person person1 = new Person("Kirill",77);
-        Person person2 = new Person("Kirill",88);
-        Person person3= new Person("Kirill",99);
-        Person person4= new Person("Kirill",12);
-        Person person5= new Person("Kirill",66);
-        Person person6= new Person("Kirill",34);
-
+        Person person = new Person("Kirill", 22);
+        Person person1 = new Person("Kirill", 77);
+        Person person2 = new Person("Kirill", 88);
+        Person person3 = new Person("Kirill", 99);
+        Person person4 = new Person("Kirill", 12);
+        Person person5 = new Person("Kirill", 66);
+        Person person6 = new Person("Kirill", 34);
 
 
         // Сортировка по возрасту
-        var list = new ArrayList<Person>(List.of(person,person1,person2,person3,person4,person5,person6));
+        var list = new ArrayList<Person>(List.of(person, person1, person2, person3, person4, person5, person6));
         list.sort(new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
                 return Integer.compare(o1.getAge(), o2.getAge());
             }
         });
-        System.out.println(list+"\n"+"\n");
-
+        System.out.println(list + "\n" + "\n");
 
 
         //Замена объектов в коллекции
-        var stringList = new ArrayList<String>(List.of("Kirill","Oleg","Kirill","Oleg","Oleg"));
-        Collections.replaceAll(stringList,"Oleg","Kate");
+        var stringList = new ArrayList<String>(List.of("Kirill", "Oleg", "Kirill", "Oleg", "Oleg"));
+        Collections.replaceAll(stringList, "Oleg", "Kate");
         System.out.println(stringList);
 
 
-        var integerList1 = new ArrayList<Integer>(List.of(1,7,0,-56,-23,77,1096,8,10,10,10));
-        System.out.println("Число 10 встречается ровно:"+Collections.frequency(integerList1,10)+" раза");
+        var integerList1 = new ArrayList<Integer>(List.of(1, 7, 0, -56, -23, 77, 1096, 8, 10, 10, 10));
+        System.out.println("Число 10 встречается ровно:" + Collections.frequency(integerList1, 10) + " раза");
 
-        var StringList = new ArrayList<String>(List.of("Kirill","Kirill","Kirill","Kirill"));
-        System.out.println("Количество вхождений строки 'Кирилл':"+Collections.frequency(StringList,"Kirill"));
-
-
+        var StringList = new ArrayList<String>(List.of("Kirill", "Kirill", "Kirill", "Kirill"));
+        System.out.println("Количество вхождений строки 'Кирилл':" + Collections.frequency(StringList, "Kirill"));
 
 
-        var testInt1 = new ArrayList<Integer>(List.of(6,8,1,0,9,6,2,3,11));
-        var testInt2 = new ArrayList<Integer>(List.of(4,2,3,11,89));
+        var testInt1 = new ArrayList<Integer>(List.of(6, 8, 1, 0, 9, 6, 2, 3, 11));
+        var testInt2 = new ArrayList<Integer>(List.of(4, 2, 3, 11, 89));
         // Если тру, то элементов общих нет
-        System.out.println(Collections.disjoint(testInt1,testInt2));
+        System.out.println(Collections.disjoint(testInt1, testInt2));
 
 
         //Удалений из 1 коллекции всех элементов, что есть во 2 коллекции
@@ -79,18 +76,9 @@ public class Main {
         testInt1.retainAll(testInt2);
         System.out.println(testInt1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
     }
+
+
+
 }
